d822290843c4411724a5549a26e4bf8caa7415de
dipandutta <116013718+dipandutta19@users.noreply.github.com>
2024-05-29 00:35:20 +0530

    Update 4.py

--- 4_server.py
+++ 4_server.py
@@ -29,3 +29,23 @@ while True:
         client_socket.sendall(response)
 
     client_socket.close()
+
+
+# Client Side 
+
+import socket
+server_ip = '127.0.0.1'
+server_port = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect((server_ip, server_port))
+
+request = "GET /date-time"
+client_socket.sendall(request.encode())
+
+response = client_socket.recv(1024)
+
+print("Current Date and Time:", response.decode())
+
+client_socket.close()
